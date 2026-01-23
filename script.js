9588d4ffdabc4889d17e6174f5c555603a8cab69
Adam Mlynarczyk <w71994@student.wsiz.edu.pl>
2026-01-23 21:32:55 +0100

    log in/out + register functionality base

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
-function log_placeholder(){ //replace with proper login subpage
-    usern = prompt("Username:","");
-    passwd = prompt("Password:","");
+const users = [["admin","admin"]];
+
+function logIn(){
+    let usern = prompt("Username:","");
+    for(let i = 0; i < users.length; i++){
+        if(users[i][0] == usern){
+            let passwd = prompt("Password:","");
+            if(users[i][1] == passwd){
+                alert("Logged in successfully!")
+                document.getElementById("user-actions").innerHTML=
+                "<button onClick=logOut()>Log out</button><div id=user>"+users[i][0]+"</div>";
+                return;
+            }
+            else{
+                alert("Incorrect password!")
+                return;
+            }
+        }
+    }
+    alert("User does not exist!")
 }
 
-window.addEventListener('load', function () {
-    if(1==1){ //no user cookie
-        document.getElementById("user-actions").innerHTML="<button onClick=log_placeholder()>Log in/Register</button>"; //convert to form?
+function register(){
+    let usern = prompt("Username:","");
+    for(let i = 0; i < users.length; i++){
+        if(users[i][0] == usern){
+            alert("User already exists!");
+            return;
+        }
     }
-    else{
-        document.getElementById("user-actions").innerHTML="Post/Log out/Username";
-    }
-})
+    let passwd = prompt("Password:","");
+    users.push([usern, passwd]);
+    alert("User created successfully!");
+}
+
+function logOut(){
+    document.getElementById("user-actions").innerHTML=
+    "<button onClick=logIn()>Log in</button><button onClick=register()>Register</button>";
+}
+
+//window.addEventListener('load', function () {
+//    document.getElementById("user-actions").innerHTML=
+//        ""; //convert to form?
+//
+//    fetch('users.txt').then(response => response.text()).then(text => console.log(text))
+//})
