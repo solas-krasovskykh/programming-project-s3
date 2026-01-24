4bee1af8a87d37893f855120b75067f486f93297
Adam Mlynarczyk <w71994@student.wsiz.edu.pl>
2026-01-24 17:13:37 +0100

    posts/users are now stored properly and persist between sessions + small fixes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let users = [["admin","admin"]];
-let posts = [["post1", "author", "cooooooooooooooooooontent"], ["post2", "differentauthor", "cooooooooooooooooooontent"]];
+let users = [];
+let posts = [];
 
-function logIn(){
+function logIn(){ //change to proper input fields?
     let usern = prompt("Username:","");
     for(let i = 0; i < users.length; i++){
         if(users[i][0] == usern){
@@ -22,7 +22,7 @@ function logIn(){
     alert("User does not exist!")
 }
 
-function register(){
+function register(){ //change to proper input fields?
     let usern = prompt("Username:","");
     for(let i = 0; i < users.length; i++){
         if(users[i][0] == usern){
@@ -33,6 +33,7 @@ function register(){
     let passwd = prompt("Password:","");
     users.push([usern, passwd]);
     alert("User created successfully!");
+    refresh();
 }
 
 function logOut(){
@@ -42,32 +43,41 @@ function logOut(){
 }
 
 function removePost(i){
-    if(i == 0)
-        posts.shift();
-    else
-        posts.splice(i, i);
+    posts.splice(i, 1);
     refresh();
 }
 
-function makePost(){
+function makePost(){ //change to proper input fields?
     let tit = prompt("Title:","");
     let cont = prompt("Post content:","");
-    posts.push([tit, document.getElementById("user").innerHTML, cont]);
+    let d = new Date();
+    posts.push([tit, document.getElementById("user").innerHTML, cont, 
+        d.getDate()+"."+(d.getMonth()+1)+"."+d.getFullYear()+", "+d.getHours()+":"+d.getMinutes()]);
     refresh();
 }
 
-function refresh(){
+function refresh(){ //refreshes the post feed + saves everything in the local storage
     document.getElementById("feed").innerHTML="";
     let hasButton="";
     for(let i = posts.length-1; i >= 0; i--){
         if(posts[i][1] == document.getElementById("user").innerHTML || document.getElementById("user").innerHTML == "admin")
             hasButton="<button onClick=removePost("+i+")>🗑️</button>";
         document.getElementById("feed").innerHTML+=
-        "<div class=post><h2>"+posts[i][0]+"</h2>"+hasButton+"<h5>"+posts[i][1]+"</h5><div class=post-content>"+posts[i][2]+"</div></div>";
+        "<div class=post><h2 class=post-title>"+posts[i][0]+"</h2>"+hasButton+
+        "<h5>"+posts[i][1]+": "+posts[i][3]+"</h5><div class=post-content>"+posts[i][2]+"</div></div>";
     }
+    localStorage.postsStorage = JSON.stringify(posts);
+    localStorage.usersStorage = JSON.stringify(users);
 }
 
 
-window.addEventListener('load', function () {
+window.addEventListener('load', function () { //creates default post + admin account on initial use / pulls data out of local storage then loads it
+    console.log(localStorage.usersStorage);
+    if(localStorage.postsStorage == undefined)
+        localStorage.postsStorage = JSON.stringify([["test", "test", "test", "test "]]);
+    if(localStorage.usersStorage == undefined)
+        localStorage.usersStorage = JSON.stringify([["admin", "admin"]]);
+    posts = JSON.parse(localStorage.postsStorage);
+    users = JSON.parse(localStorage.usersStorage);
     refresh();
 })
